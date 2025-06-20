e76621f3840a75a5b2b8bb45b2d6af259a515d5e
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-20 11:03:38 +0530

    Q2

--- LeetCode_BusinessLogic_SqlServer.sql
+++ LeetCode_BusinessLogic_SqlServer.sql
@@ -41,3 +41,47 @@ select p.product_id , ISNULL(round(CAST(SUM(u.units * p.price) AS FLOAT) / NULLI
 left join UnitsSold u on p.product_id = u.product_id
 and u.purchase_date between p.start_date and p.end_date
 group by p.product_id
+
+
+/*2. Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input: 
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+Output: 
++-------------+---------------+
+| project_id  | average_years |
++-------------+---------------+
+| 1           | 2.00          |
+| 2           | 2.50          |
++-------------+---------------+
+Explanation: The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50 */
+select p.project_id, round(avg(cast(e.experience_years as decimal(10,2))),2) as average_years from Project p
+left join Employee e on p.employee_id = e.employee_id
+group by p.project_id
+order by p.project_id
+
+
