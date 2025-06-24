2e824c97c9a040ad74ba565b942c302cbbc2e2ba
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-24 12:20:26 +0530

    Q10

--- LeetCode_BusinessLogic_SqlServer.sql
+++ LeetCode_BusinessLogic_SqlServer.sql
@@ -399,3 +399,42 @@ Teacher 2:
   - They teach subject 4 in department 1.*/
 select teacher_id, count(distinct subject_id) as cnt from Teacher
 group by teacher_id
+
+/*Q10. Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+Output: 
++------------+--------------+ 
+| day        | active_users |
++------------+--------------+ 
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
++------------+--------------+ */
+select activity_date as day, count(distinct user_id) as active_users from Activity
+where activity_date between dateadd(day, -29,'2019-07-27') and '2019-07-27'
+group by activity_date;
+
