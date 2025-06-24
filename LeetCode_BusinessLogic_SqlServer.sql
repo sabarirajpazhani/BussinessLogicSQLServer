462afaada0e5bf729d66f3858515abf0da40f59b
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-24 17:19:49 +0530

    12

--- LeetCode_BusinessLogic_SqlServer.sql
+++ LeetCode_BusinessLogic_SqlServer.sql
@@ -476,3 +476,75 @@ with products as(
 select product_id, year as first_year, quantity, price from products 
 where rnk = 1;
 
+
+/*Q12. Write a solution to:
+
+Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Movies table:
++-------------+--------------+
+| movie_id    |  title       |
++-------------+--------------+
+| 1           | Avengers     |
+| 2           | Frozen 2     |
+| 3           | Joker        |
++-------------+--------------+
+Users table:
++-------------+--------------+
+| user_id     |  name        |
++-------------+--------------+
+| 1           | Daniel       |
+| 2           | Monica       |
+| 3           | Maria        |
+| 4           | James        |
++-------------+--------------+
+MovieRating table:
++-------------+--------------+--------------+-------------+
+| movie_id    | user_id      | rating       | created_at  |
++-------------+--------------+--------------+-------------+
+| 1           | 1            | 3            | 2020-01-12  |
+| 1           | 2            | 4            | 2020-02-11  |
+| 1           | 3            | 2            | 2020-02-12  |
+| 1           | 4            | 1            | 2020-01-01  |
+| 2           | 1            | 5            | 2020-02-17  | 
+| 2           | 2            | 2            | 2020-02-01  | 
+| 2           | 3            | 2            | 2020-03-01  |
+| 3           | 1            | 3            | 2020-02-22  | 
+| 3           | 2            | 4            | 2020-02-25  | 
++-------------+--------------+--------------+-------------+
+Output: 
++--------------+
+| results      |
++--------------+
+| Daniel       |
+| Frozen 2     |
++--------------+
+Explanation: 
+Daniel and Monica have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
+Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.*/
+with userRating as (
+    select mr.user_id, u.name, count(mr.user_id) as cnt, dense_rank() over(order by count(mr.user_id) desc, name) as rnk1 from MovieRating mr
+	inner join Users u on mr.user_id = u.user_id
+	group by mr.user_id, u.name
+),
+movie as(
+	select mr.movie_id, m.title, avg(cast(mr.rating as float)) as avgRating, dense_rank() over(order by avg(cast(mr.rating as float)) desc, title) as rnk2 from MovieRating mr
+	inner join Movies m on mr.movie_id = m.movie_id
+	where year(mr.created_at) = '2020' and month(mr.created_at) = '02'
+	group by mr.movie_id,m.title
+),
+result as(
+	select userRating.name as results from userRating
+	where rnk1 = 1
+	union all
+	select  movie.title from movie
+	where rnk2 =1
+)
+select * from result
