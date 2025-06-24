700d871c0404184955c9975a729d2e305397cb06
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-24 12:54:11 +0530

    Update LeetCode_BusinessLogic_SqlServer.sql

--- LeetCode_BusinessLogic_SqlServer.sql
+++ LeetCode_BusinessLogic_SqlServer.sql
@@ -438,3 +438,41 @@ select activity_date as day, count(distinct user_id) as active_users from Activi
 where activity_date between dateadd(day, -29,'2019-07-27') and '2019-07-27'
 group by activity_date;
 
+/*Q11. Write a solution to find all sales that occurred in the first year each product was sold.
+
+For each product_id, identify the earliest year it appears in the Sales table.
+
+Return all sales entries for that product in that year.
+
+Return a table with the following columns: product_id, first_year, quantity, and price.
+Return the result in any order.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+*/
+with products as(
+    select product_id, year, quantity, price , dense_rank() over (partition by product_id order by year) as rnk from Sales 
+    group by product_id, year, quantity, price
+)
+
+select product_id, year as first_year, quantity, price from products 
+where rnk = 1;
+
